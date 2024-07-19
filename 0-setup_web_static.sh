762263cdcf018b5daaa6db345a37dc8212f0e433
Iyaz <moseseniade@gmail.com>
2024-07-19 03:44:29 -0700

    My exit code is fine, whats the error

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,12 +1,27 @@
 #!/usr/bin/env bash
-# sets up the web servers for the deployment of web_static
+#Sets up the webserver for deployment
 
-sudo apt-get -y update
-sudo apt-get -y upgrade
-sudo apt-get -y install nginx
-sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
-echo "This is a test" | sudo tee /data/web_static/releases/test/index.html
+sudo apt-get update
+sudo apt-get install -y nginx
+sudo service nginx start
+
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+
+echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>
+" | sudo tee /data/web_static/releases/test/index.html > /dev/null
+
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown -hR ubuntu:ubuntu /data/
-sudo sed -i '13i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+
+sudo chown -R ubuntu:ubuntu /data/
+
+dataloc='\\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}'
+sudo sed -i "43i ${dataloc}" /etc/nginx/sites-available/default
+
 sudo service nginx restart
