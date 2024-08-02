8b6c5525e004db29a545fbcb52340491b73c676b
iyaz07 <moseseniade@gmail.com>
2024-08-02 14:10:14 -0700

    Update state

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -69,3 +69,7 @@ class FileStorage:
             pass
         except KeyboardInterrupt:
             pass
+
+     def close(self):
+        """call reload() method for deserializing the JSON file to objects"""
+        self.reload()
