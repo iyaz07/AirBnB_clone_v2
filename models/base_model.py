240758ccf47b7940d940c07ddf7f5dd8ca577b7b
iyaz07 <moseseniade@gmail.com>
2024-08-02 23:39:33 -0700

    Fix save and to_dict

--- models/base_model.py
+++ models/base_model.py
@@ -3,53 +3,73 @@
 import uuid
 from datetime import datetime
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import Column, String, DateTime
 
+time = "%Y-%m-%dT%H:%M:%S.%f"
+
+# Create a base class for all models
 Base = declarative_base()
 
 class BaseModel:
     """A base class for all hbnb models"""
-    id = Column(String(60), primary_key=True, nullable=False)
-    created_at = Column(DateTime, nullable=False, default=datetime.now())
-    updated_at = Column(DateTime, nullable=False, default=datetime.now())
+
+    # Define columns for the base model
+    id = Column(String(60), primary_key=True, nullable=False, default=str(uuid.uuid4()))
+    created_at = Column(DateTime, nullable=False, default=datetime.utcnow)
+    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow)
 
     def __init__(self, *args, **kwargs):
-        """Instatntiates a new model"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-        
+        """Instantiates a new model"""
+        # Initialize default values for id, created_at, and updated_at
+        if not kwargs.get('id'):
+            self.id = str(uuid.uuid4())
+        if not kwargs.get('created_at'):
+            self.created_at = datetime.utcnow()
+        if not kwargs.get('updated_at'):
+            self.updated_at = datetime.utcnow()
+
+        # Update attributes from kwargs if provided
         if kwargs:
             for key, value in kwargs.items():
-                if key == 'created_at' or key == 'update_at':
+                if key in ['created_at', 'updated_at']:
+                    # Convert string to datetime if necessary
                     value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
-                    if key != '__class__' and hasattr(self.__class__, key):
-                        setattr(self, key, value)
+                if key != '__class__' and hasattr(self.__class__, key):
+                    setattr(self, key, value)
 
     def __str__(self):
-        """Returns a string representation of the instance"""
-        cls = (str(type(self)).split('.')[-1]).split('\'')[0]
-        return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
+        """String representation of the BaseModel class"""
+        return "[{:s}] ({:s}) {}".format(self.__class__.__name__, self.id, self.to_dict())
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
         from models import storage
-        self.updated_at = datetime.now()
+        self.updated_at = datetime.utcnow()
         storage.new(self)
         storage.save()
 
     def to_dict(self):
-        """Convert instance into dict format"""
-        dictionary = {}
-        dictionary.update(self.__dict__)
-        dictionary.update({'__class__':
-                          (str(type(self)).split('.')[-1]).split('\'')[0]})
-        dictionary['created_at'] = self.created_at.isoformat()
-        dictionary['updated_at'] = self.updated_at.isoformat()
-        dictionary.pop('_sa_instance_state', None)
-        return dictionary
+        """returns a dictionary containing all keys/values of the instance"""
+        new_dict = self.__dict__.copy()
+        if "created_at" in new_dict:
+            new_dict["created_at"] = new_dict["created_at"].strftime(time)
+        if "updated_at" in new_dict:
+            new_dict["updated_at"] = new_dict["updated_at"].strftime(time)
 
-    def delete(self):
-        """Deletes the instance from the storage"""
-        from models import storage
-        storage.delete(self)
+        # Move 'name' to the first position in the dictionary if it exists
+        if "name" in new_dict:
+            name_value = new_dict.pop("name")
+            new_dict = {"name": name_value, **new_dict}
+
+        # Remove SQLAlchemy instance state
+        if "_sa_instance_state" in new_dict:
+            del new_dict["_sa_instance_state"]
+
+        return new_dict
+
+    def to_dict_with_class(self):
+        """returns a dictionary including the class name"""
+        new_dict = self.to_dict()
+        new_dict["__class__"] = self.__class__.__name__
+        return new_dict
+
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -21,55 +21,46 @@ class FileStorage:
             if isinstance(cls, str):
                 cls = globals().get(cls)
             if cls and issubclass(cls, BaseModel):
-                cls_dict = {k: v for k,
-                            v in self.__objects.items() if isinstance(v, cls)}
+                cls_dict = {k: v for k, v in self.__objects.items() if isinstance(v, cls)}
                 return cls_dict
         return FileStorage.__objects
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
-        self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
+        key = f"{obj.__class__.__name__}.{obj.id}"
+        self.__objects[key] = obj
 
     def save(self):
         """Saves storage dictionary to file"""
         with open(FileStorage.__file_path, 'w') as f:
-            temp = {}
-            temp.update(FileStorage.__objects)
-            for key, val in temp.items():
-                temp[key] = val.to_dict()
+            temp = {key: val.to_dict_with_class() for key, val in FileStorage.__objects.items()}
             json.dump(temp, f)
 
     def reload(self):
         """Loads storage dictionary from file"""
         classes = {
-                    'BaseModel': BaseModel, 'User': User, 'Place': Place,
-                    'State': State, 'City': City, 'Amenity': Amenity,
-                    'Review': Review
-                  }
+            'BaseModel': BaseModel, 'User': User, 'Place': Place,
+            'State': State, 'City': City, 'Amenity': Amenity,
+            'Review': Review
+        }
         try:
-            temp = {}
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                    cls = classes.get(val['__class__'])
+                    if cls:
+                        self.__objects[key] = cls(**val)
         except FileNotFoundError:
-            pass
-        except KeyboardInterrupt:
             pass
 
     def delete(self, obj=None):
         """Deletes an object from storage"""
-        if obj is None:
-            return
-        to_del = f"{obj.__class__.__name__}.{obj.id}"
-
-        try:
-            del FileStorage.__objects[to_del]
-        except AttributeError:
-            pass
-        except KeyboardInterrupt:
-            pass
+        if obj:
+            key = f"{obj.__class__.__name__}.{obj.id}"
+            if key in self.__objects:
+                del self.__objects[key]
 
     def close(self):
-        """call reload() method for deserializing the JSON file to objects"""
+        """Call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
